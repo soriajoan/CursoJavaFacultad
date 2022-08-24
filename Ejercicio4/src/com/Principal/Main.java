d211457046ef4105aef993e0a09d292607cb13e8
soriajoan <109100570+soriajoan@users.noreply.github.com>
2022-08-24 20:41:10 -0300

    Add files via upload

---
+++ Ejercicio4/src/com/Principal/Main.java
@@ -0,0 +1,22 @@
+package com.Principal;
+
+
+	public class Main {
+
+           public static void main(String[] args) {
+
+                String nombre;
+                int edad;
+                double salario;
+                boolean carnet;
+
+                nombre = "Joan";
+                edad = 20;
+                salario = 9999.99;
+                carnet = true;
+
+                System.out.println("Nombre:"+nombre +"\n" +"Edad:"+ edad + "\n"+"Salario:"+ salario + "\n"+"Carnet:" + carnet + "\n");
+            }
+
+    }
+
