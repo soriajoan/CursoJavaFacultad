23a373a9a65d8c80d7070d4d9e614bc195ef1cfc
soriajoan <109100570+soriajoan@users.noreply.github.com>
2022-08-24 21:15:51 -0300

    Add files via upload

---
+++ Ejercicio6/src/com/Principal/Main.java
@@ -0,0 +1,31 @@
+package com.Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a1,a2,a3,a4,a5,a6,a7,a8,a9;
+   //Funciones trigonométricas habituales
+        a1=  Math.sin(0);
+        a2 = Math.cos(0);
+        a3 = Math.tan(30);
+        a4 = Math.atan(30);
+        a5 = Math.atan2(20,30);
+    //Función exponencial y su inversa
+        a6 = Math.exp(2);
+        a7 = Math.log(2);
+    //Constantes PI y e
+        a8 = Math.PI;
+        a9 = Math.E;
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        System.out.println(a9);
+    }
+}
