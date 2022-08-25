58656b491ad0e92b82ec2519e6d444025562f146
soriajoan <109100570+soriajoan@users.noreply.github.com>
2022-08-24 21:02:56 -0300

    Add files via upload

---
+++ Ejercicio 5/Tp5UnEjercicioCombinado/src/com/Principal/Main.java
@@ -0,0 +1,17 @@
+package com.Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double resultado,a,b,c,d,e,f;
+        a=6.0/5.0;
+        b=2.0/3.0;
+        c=7.0/2.0;
+        d=2.0/30.0;
+        e=1.0/3.0;
+        f=5.0;
+        resultado = (a-b*c+d)/(e/f);
+
+        System.out.println("Resultado = " + resultado);
+    }
+}
